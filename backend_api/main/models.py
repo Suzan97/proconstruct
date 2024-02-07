94333a6f41729a0bd3c81019d1ca6b868a48e48e
SueWaf <susannafula.sw@gmail.com>
2024-02-07 15:40:24 +0300

    update models

--- backend_api/main/models.py
+++ backend_api/main/models.py
@@ -43,7 +43,7 @@ class Product(models.Model):
 #Customer Model
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    mobile = models.PositiveBigIntegerField(null=True)
+    mobile = models.PositiveBigIntegerField(unique=True)
 
     def __str__(self):
         return self.user.username
